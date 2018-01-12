e2592cd5dd957c040c90341d8a7a492bd2e26e04
Ch3shireDev <thesmilingcatofcheshire@gmail.com>
2018-01-12 10:06:36 +0100

    Added more Bootstrap and W3CSS examples

--- ex/example1.php
+++ ex/example1.php
@@ -2,7 +2,61 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Example</title>
-  <link rel="stylesheet" type="text/css" href="example1.css">
+    <style type="text/css">
+        body {
+            /*border: 1px solid;*/
+            /*display: flex;*/
+            margin: 0;
+        }
+
+            body * {
+                border: 1px solid;
+                /*position: relative;*/
+                box-sizing: border-box;
+            }
+
+        /*
+    The box-sizing property defines how the width and height 
+    of an element are calculated: should they include padding 
+    and borders, or not.
+
+    content-box 	Default. The width and height properties 
+                    (and min/max properties) includes only the 
+                    content. Border and padding are not included
+
+    border-box 	    The width and height properties (and min/max 
+                    properties) includes content, 
+                    padding and border
+*/
+
+        .column {
+            width: 50%;
+            float: left;
+        }
+
+        .left, .right {
+            /*width: 250px;*/
+            padding: 10px;
+            min-height: 100vh;
+            /*height: 100vh;*/
+        }
+
+        .left {
+            float: left;
+            width: 75%;
+        }
+
+        .right {
+            background: green;
+            width: 25%;
+        }
+
+        .header {
+        }
+
+        .content {
+        }
+    </style>
 </head>
 <body>
     <div class="column">
